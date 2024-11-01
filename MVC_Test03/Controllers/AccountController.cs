2304e39d01ad025f8d9df871c12d1aecd29125ed
Trần Anh Duy <23DH110527@st.huflit.edu.vn>
2024-11-01 18:22:22 +0700

    Add project files.

---
+++ MVC_Test03/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using MVC_Test03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC_Test03.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: SignIn
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AdminUser user)
+        {
+            using (var db = new DBSportStoreEntities())
+            {
+                var validUser = db.AdminUsers.FirstOrDefault(u => u.NameUser == user.NameUser && u.PasswordUser == user.PasswordUser);
+                if (validUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(validUser.NameUser, false);
+                    Session["Role"] = validUser.RoleUser;  // Lưu vai trò người dùng trong session
+                    return RedirectToAction("ProductList", "Products");
+                }
+                ViewBag.ErrorMessage = "Invalid Username or Password";
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session["Role"] = null;  // Xóa session vai trò người dùng
+            return RedirectToAction("Login", "Home");
+        }
+
+    }
+}
---
+++ MVC_Test03/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using MVC_Test03.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace MVC_Test03.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DBSportStoreEntities db = new DBSportStoreEntities();
+        public ActionResult Index(int? page)
+        {
+        
+            // Khai báo mỗi trang có 8 sản phẩm
+            int pageSize = 8;
+            int pageNumber = (page ?? 1);
+            var selectedAQTDNames = new List<string>
+    {
+        "Bộ Bàn Ghế Phòng Ăn",
+        "Bàn Ăn Dá Cẩm Thạch",
+       "Bàn Ăn Nhập Khẩu ARETI-102"
+    };
+             var products = db.Products
+             .Where(p => selectedAQTDNames.Contains(p.NamePro))
+                     .OrderBy(x => Guid.NewGuid()) // Chọn ngẫu nhiên
+                     .ToPagedList(pageNumber, pageSize); // Phân trang
+
+
+            return View(products);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
